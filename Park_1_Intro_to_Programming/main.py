6cfb6b4270c9cb64349219ee71590405bf6d900d
Alfadjri Dwi Fadhilah <alfadjri28@gmail.com>
2025-05-02 14:50:06 +0700

    Pertemuan 1
    membuat aplikasi Selamat Datang

---
+++ Park_1_Intro_to_Programming/main.py
@@ -0,0 +1,5 @@
+# Varibel adalah tempat penampungan sementara dan sifatnya hanya bisa menyimpan 1 nilai saja
+# namaVariable = nilai yang mau di isikan 
+# input adalah code untuk mengambil apa yang di ketik sama user
+nama_lengkap = input("Masukan nama Anda : ")
+print(f"Helo , {nama_lengkap}\nSelamat Datang di program pertama saya")
